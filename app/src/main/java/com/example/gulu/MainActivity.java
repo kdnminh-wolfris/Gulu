1076d42ca0ba851df645ad4532219818aac49dc9
kdnminh-wolfris <khaudangnhatminh@gmail.com>
2021-07-10 18:12:21 +0700

    fix bug loading too large image file

--- app/src/main/java/com/example/gulu/MainActivity.java
+++ app/src/main/java/com/example/gulu/MainActivity.java
@@ -7,7 +7,11 @@ import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
 import android.content.Intent;
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -16,19 +20,32 @@ public class MainActivity extends AppCompatActivity {
     private ImageView btnCamera;
     private ImageView btnGallery;
     private ImageView btnLibrary;
+    private int btnDelayTime = 100; //miliseconds
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        loadDecodedImage(R.id.gulu_logo, R.drawable.gulu_logo, 196, 100);
+        loadDecodedImage(R.id.star_line, R.drawable.star_line, 271, 60);
+        loadDecodedImage(R.id.btn_camera, R.drawable.camera, 211, 113);
+        loadDecodedImage(R.id.btn_gallery, R.drawable.gallery, 211, 113);
+        loadDecodedImage(R.id.btn_library, R.drawable.library, 211, 113);
+
         btnCamera = findViewById(R.id.btn_camera);
         btnCamera.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                btnCamera.setImageResource(R.drawable.camera_pressed);
-                openCameraActivity();
-                btnCamera.setImageResource(R.drawable.camera);
+                loadDecodedImage(R.id.btn_camera, R.drawable.camera_pressed, 211, 113);
+                final Handler handler = new Handler();
+                handler.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        openCameraActivity();
+                        loadDecodedImage(R.id.btn_camera, R.drawable.camera, 211, 113);
+                    }
+                }, btnDelayTime);
             }
         });
 
@@ -36,9 +53,15 @@ public class MainActivity extends AppCompatActivity {
         btnGallery.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                btnGallery.setImageResource(R.drawable.gallery_pressed);
-                openGalleryActivity();
-                btnGallery.setImageResource(R.drawable.gallery);
+                loadDecodedImage(R.id.btn_gallery, R.drawable.gallery_pressed, 211, 113);
+                final Handler handler = new Handler();
+                handler.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        openGalleryActivity();
+                        loadDecodedImage(R.id.btn_gallery, R.drawable.gallery, 211, 113);
+                    }
+                }, btnDelayTime);
             }
         });
 
@@ -46,11 +69,57 @@ public class MainActivity extends AppCompatActivity {
         btnLibrary.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                btnLibrary.setImageResource(R.drawable.library_pressed);
-                openLibraryActivity();
-                btnLibrary.setImageResource(R.drawable.library);
+                loadDecodedImage(R.id.btn_library, R.drawable.library_pressed, 211, 113);
+                final Handler handler = new Handler();
+                handler.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        openLibraryActivity();
+                        loadDecodedImage(R.id.btn_library, R.drawable.library, 211, 113);
+                    }
+                }, btnDelayTime);
             }
         });
+    }
+
+    private void loadDecodedImage(int imageViewId, int imageId, int width, int height) {
+        ImageView imageView = findViewById(imageViewId);
+        imageView.setImageBitmap(decodeSampleBitmapFromResource(getResources(), imageId, width, height));
+    }
+
+    private Bitmap decodeSampleBitmapFromResource(Resources res, int resId, int reqWidth, int reqHeight) {
+        // First decode with inJustDecodeBounds=true to check dimensions
+        final BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inJustDecodeBounds = true;
+        BitmapFactory.decodeResource(res, resId, options);
+
+        // Calculate inSampleSize
+        options.inSampleSize = calculateInSampleSize(options, reqWidth, reqHeight);
+
+        // Decode bitmap with inSampleSize set
+        options.inJustDecodeBounds = false;
+        return BitmapFactory.decodeResource(res, resId, options);
+    }
+
+    public static int calculateInSampleSize(BitmapFactory.Options options, int reqWidth, int reqHeight) {
+        // Raw height and width of image
+        final int height = options.outHeight;
+        final int width = options.outWidth;
+        int inSampleSize = 1;
+
+        if (height > reqHeight || width > reqWidth) {
+
+            final int halfHeight = height / 2;
+            final int halfWidth = width / 2;
+
+            // Calculate the largest inSampleSize value that is a power of 2 and keeps both
+            // height and width larger than the requested height and width.
+            while ((halfHeight / inSampleSize) >= reqHeight
+                    && (halfWidth / inSampleSize) >= reqWidth) {
+                inSampleSize *= 2;
+            }
+        }
+        return inSampleSize;
     }
 
     private void openLoadingActivity() {
